309df0655c5b707103a290791e3c8a2d54c0cbd7
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-10-31 19:31:11 -0400

    Finished core, bugfixing

--- ReactJS_Conway/react-gameoflife/src/Game.js
+++ ReactJS_Conway/react-gameoflife/src/Game.js
@@ -31,11 +31,58 @@ class Game extends React.Component {
     }
   };
 
+  calculateNeighbors(board, x, y) {
+    let neighbors = 0;
+    const dirs = [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, -1]
+    ];
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      let y1 = y + dir[0];
+      let x1 = x + dir[1];
+
+      if (
+        x1 >= 0 &&
+        x1 < this.cols &&
+        y1 >= 0 &&
+        y1 < this.rows &&
+        board[y1][x1]
+      ) {
+        neighbors++;
+      }
+    }
+
+    return neighbors;
+  }
+
   runIteration() {
-    console.log("running iteration");
     let newBoard = this.makeEmptyBoard();
 
-    //TODO: Add iteration logic here
+    //Iteration logic
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let neighbors = this.calculateNeighbors(this.board, x, y);
+        if (this.board[y][x]) {
+          if (neighbors === 2 || neighbors === 3) {
+            newBoard[y][x] = true;
+          } else {
+            newBoard[y][x] = false;
+          }
+        } else {
+          if (!this.board[y][x] && neighbors === 3) {
+            newBoard[y][x] = true;
+          }
+        }
+      }
+    }
+    ///////////
 
     this.board = newBoard;
     this.setState({ cells: this.makeCells() });
@@ -64,9 +111,9 @@ class Game extends React.Component {
   //create cells from this.board array
   makeCells() {
     let cells = [];
-    for (let x = 0; x < this.rows; x++) {
-      for (let y = 0; y < this.cols; y++) {
-        if (this.board[x][y]) {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        if (this.board[y][x]) {
           cells.push({ x, y });
         }
       }
@@ -92,15 +139,16 @@ class Game extends React.Component {
     console.log(offsetX);
     const x = Math.floor(offsetX / Constants.CELL_SIZE);
     const y = Math.floor(offsetY / Constants.CELL_SIZE);
-    console.log(y);
+
     if (x > 0 && x < this.cols && y >= 0 && y <= this.rows) {
-      this.board[x][y] = !this.board[x][y];
+      console.log(x, y);
+      this.board[y][x] = !this.board[y][x];
     }
 
     this.setState({ cells: this.makeCells() });
   };
   render() {
-    const { cells } = this.state;
+    const { cells, interval, isRunning } = this.state;
     return (
       <div>
         <div
@@ -125,7 +173,7 @@ class Game extends React.Component {
             onChange={this.handleIntervalChange}
           />{" "}
           msec{" "}
-          {this.isRunning ? (
+          {isRunning ? (
             <button className="button" onClick={this.stopGame}>
               Stop
             </button>
