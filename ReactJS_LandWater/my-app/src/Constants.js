e2cb763de13d6cedc734b35b4a7374f9cb1160b7
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-11-07 14:02:20 -0500

    Basic game working

--- ReactJS_LandWater/my-app/src/Constants.js
+++ ReactJS_LandWater/my-app/src/Constants.js
@@ -1,5 +1,5 @@
 export const CELL_SIZE = 100;
-export const WIDTH = 800;
-export const HEIGHT = 600;
+export const WIDTH = 1200;
+export const HEIGHT = 800;
 
 console.log(CELL_SIZE, WIDTH, HEIGHT);
--- ReactJS_LandWater/my-app/src/Game.js
+++ ReactJS_LandWater/my-app/src/Game.js
@@ -9,10 +9,13 @@ class Game extends React.Component {
     this.cols = Constants.WIDTH / Constants.CELL_SIZE;
     this.board = this.makeStartState();
     this.squares = this.makeLevel();
+    this.targetRatio = this.makeTargetRatio();
   }
 
   state = {
-    squares: []
+    squares: [],
+    victory: false,
+    targetRatio: {}
   };
 
   calculateNeighbors(board, x, y) {
@@ -53,9 +56,9 @@ class Game extends React.Component {
       board[y] = [];
       for (let x = 0; x < this.cols; x++) {
         if (y === this.rows - 1 && x === this.cols - 1) {
-          board[y][x] = "end fas fa-building";
+          board[y][x] = "end fas fa-building land";
         } else if (y == 0 && x == 0) {
-          board[y][x] = "start fas fa-truck ";
+          board[y][x] = "start fas fa-truck land";
         } else {
           board[y][x] = Math.random() < 0.5 ? "land" : "water";
         }
@@ -87,12 +90,20 @@ class Game extends React.Component {
     };
   }
 
+  makeTargetRatio = () => {
+    let workingSquares = this.cols * this.rows - 2;
+    let waterTarget = Math.floor(Math.random() * workingSquares);
+    let landTarget = workingSquares - waterTarget + 2;
+    console.log(waterTarget, landTarget);
+    return { waterTarget, landTarget };
+  };
+
   calculateRatio = () => {
     let waterTot = 0;
     let landTot = 0;
     for (let y = 0; y < this.rows; y++) {
       for (let x = 0; x < this.cols; x++) {
-        if (this.board[y][x] == "land") {
+        if (this.board[y][x].includes("land")) {
           landTot++;
         } else {
           waterTot++;
@@ -101,6 +112,63 @@ class Game extends React.Component {
     }
     return { waterTot, landTot };
   };
+
+  //see if player won
+  checkGameState = event => {
+    this.connectionLookup = [];
+    if (
+      this.targetRatio.landTarget === this.calculateRatio().landTot &&
+      this.targetRatio.waterTarget === this.calculateRatio().waterTot &&
+      this.completeConnection(0, 0)
+    ) {
+      console.log("You win!");
+    }
+    this.setState({ squares: this.makeLevel() });
+  };
+
+  connectionLookup = [];
+
+  completeConnection(x, y) {
+    const dirs = [[0, 1], [1, 0], [-1, 0], [0, -1]];
+    for (let i = 0; i < dirs.length; i++) {
+      const dir = dirs[i];
+      let y1 = y + dir[0];
+      let x1 = x + dir[1];
+      if (!this.connectionLookup.includes(`${x1}, ${y1}`)) {
+        if (
+          x1 >= 0 &&
+          x1 < this.cols &&
+          y1 >= 0 &&
+          y1 < this.rows &&
+          this.board[y1][x1].includes("land")
+        ) {
+          console.log(
+            "checking for connection from",
+            x,
+            ",",
+            y,
+            this.board[y][x],
+            "to",
+            x1,
+            ",",
+            y1,
+            this.board[y1][x1]
+          );
+
+          if (this.board[y1][x1].includes("end")) {
+            console.log("Won!");
+            return true;
+          } else {
+            this.connectionLookup.push(`${x}, ${y}`);
+            console.log(this.connectionLookup);
+            this.completeConnection(x1, y1);
+            this.board[y1][x1] = this.board[y1][x1] + " fas fa-circle";
+          }
+        }
+      }
+    }
+    return false;
+  }
 
   //handleCLick Event
   handleClick = event => {
@@ -119,7 +187,7 @@ class Game extends React.Component {
         this.board[y][x] = "land";
       }
     }
-
+    console.log(this);
     this.setState({ squares: this.makeLevel() });
   };
 
@@ -148,12 +216,22 @@ class Game extends React.Component {
             />
           ))}
         </div>
-        <div className="ratio-status">
-          Current ratio:{" "}
-          {" " +
-            this.calculateRatio().landTot +
-            " to " +
-            this.calculateRatio().waterTot}
+        <div className="controls">
+          <div className="ratio-goal">
+            Target ratio:{" "}
+            {" " +
+              this.targetRatio.landTarget +
+              " to " +
+              this.targetRatio.waterTarget}
+          </div>
+          <div className="ratio-status">
+            Current ratio:{" "}
+            {" " +
+              this.calculateRatio().landTot +
+              " to " +
+              this.calculateRatio().waterTot}
+          </div>
+          <button onClick={this.checkGameState}>Drive!</button>
         </div>
       </div>
     );
