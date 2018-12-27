e629a8da8cc3cef4ba3afa4b2fb51a5d486ef639
adagar <adagar121987@gmail.com>
2018-12-27 17:43:12 -0500

    Added babel compilation

--- Essential_Exercises/Exercise Files/Ch02/02_01/start/dist/index.js
+++ Essential_Exercises/Exercise Files/Ch02/02_01/start/dist/index.js
@@ -1,16 +1,27 @@
-const { createElement } = React
 const { render } = ReactDOM
 
+/*
+const style = {
+    backgroundColor: "orange",
+    color: "white",
+    fontFamily: "verdana"
+}
+*/
 
+/*
 const title = createElement(
     'h1',
     {id: 'title',
     className: 'header',
+    style: style
     },
     'Hello world'
 )
+*/
 
 render(
-    title,
+    <h1 id="title" className="header" style = {{ backgroundColor: "orange", color: "white", fontFamily:"verdana"}}>
+        Hello world
+    </h1>,
     document.getElementById("react-container")
 )
