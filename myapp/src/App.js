348c8b7d8b5fd5875bb4de8af9ebcd47b3124f71
adagar <adagar121987@gmail.com>
2019-01-13 17:25:15 -0500

    Deleting from array example using filter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
 
 class App extends Component {
   state = {
     ninjas: [
       { name: "Ryu", age: 30, belt: "black", id: 1 },
       { name: "Nick", age: 31, belt: "red", id: 2 },
-      { name: "Vero", age: 32, belt: "yellow", id: 3 }
+      { name: "Vero", age: 32, belt: "yellow", id: 3 },
+      { name: "Ripley", age: 38, belt: "yellow", id: 4 }
     ]
+  };
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({ ninjas: ninjas });
+  };
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter((ninja) => {
+      return ninja.id !== id;
+    });
+
+    this.setState({ ninjas: ninjas });
   };
 
   render() {
@@ -15,7 +31,8 @@ class App extends Component {
       <div className="App">
         <h1>My first react app</h1>
         <p>Welcome!</p>
-        <Ninjas ninjas={this.state.ninjas} />
+        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
+        <AddNinja addNinja={this.addNinja} />
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,17 +3,37 @@ import React from "react";
 //converting from container component to UI component by converting from class to function
 //const Ninjas = (props) => {
 //can destructure right at import like this
-const Ninjas = ({ ninjas }) => {
+const Ninjas = ({ ninjas, deleteNinja }) => {
   //class Ninjas extends Component {
   //console.log(this.props);
+  //   const ninjaList = ninjas.map((ninja) => {
+  //     if (ninja.age > 20) {
+  //       return (
+  //         <div className="ninja" key={ninja.id}>
+  //           <div>Name: {ninja.name}</div>
+  //           <div>Age: {ninja.age}</div>
+  //           <div>Belt: {ninja.belt}</div>
+  //         </div>
+  //       );
+  //     } else {
+  //       return null;
+  //     }
+  //   });
   const ninjaList = ninjas.map((ninja) => {
-    return (
+    return ninja.age > 31 ? (
       <div className="ninja" key={ninja.id}>
         <div>Name: {ninja.name}</div>
         <div>Age: {ninja.age}</div>
         <div>Belt: {ninja.belt}</div>
+        <button
+          onClick={() => {
+            deleteNinja(ninja.id);
+          }}
+        >
+          Delete me!
+        </button>
       </div>
-    );
+    ) : null;
   });
   return <div className="ninja-list">{ninjaList}</div>;
 };
