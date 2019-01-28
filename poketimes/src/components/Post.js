24055466c3b4ee5e7401569fe9d31390aa70f603
adagar <adagar121987@gmail.com>
2019-01-28 17:24:23 -0500

    31 Using axios and route paramters for a post detail page

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Post extends Component {
   state = {
-    id: null
+    post: null
   };
   componentDidMount() {
     let id = this.props.match.params.post_id;
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/" + id)
+      .then((res) => {
+        this.setState({
+          post: res.data
+        });
+        console.log(res);
+      });
     this.setState({ id });
   }
   render() {
-    return (
-      <div className="container">
-        <h4>route param</h4>
-        <p>{this.state.id}</p>
+    const post = this.state.post ? (
+      <div className="post">
+        <div className="center">
+          <h4>{this.state.post.title}</h4>
+          <p>{this.state.post.body}</p>
+        </div>
       </div>
+    ) : (
+      <div className="center">Loading post...</div>
     );
+    return <div className="container">{post}</div>;
   }
 }
 
