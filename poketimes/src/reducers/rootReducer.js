f0ae4b4d25ca10b45d2eec6ada3cfd0ad3ca3db0
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2019-02-03 14:25:48 -0500

    40 Imported { connect } from react-redux, and subscribing
    after importing connect we wrapped Home in a higher order component that it returned. Passed a local function as an argument that gets called when state updates like export default connect(mapStateToProps)(Home)

--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,5 +1,24 @@
 const initState = {
-  posts: []
+  posts: [
+    {
+      id: "1",
+      title: "some pumpkins harvested",
+      body:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, labore corrupti qui reprehenderit amet odio delectus, illum voluptatum sequi consequuntur doloremque exercitationem eaque ab. Aliquid dicta at aspernatur alias consequuntur?"
+    },
+    {
+      id: "2",
+      title: "milk the cows",
+      body:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, labore corrupti qui reprehenderit amet odio delectus, illum voluptatum sequi consequuntur doloremque exercitationem eaque ab. Aliquid dicta at aspernatur alias consequuntur?"
+    },
+    {
+      id: "3",
+      title: "chop some trees down",
+      body:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, labore corrupti qui reprehenderit amet odio delectus, illum voluptatum sequi consequuntur doloremque exercitationem eaque ab. Aliquid dicta at aspernatur alias consequuntur?"
+    }
+  ]
 };
 
 const rootReducer = (state = initState, action) => {
