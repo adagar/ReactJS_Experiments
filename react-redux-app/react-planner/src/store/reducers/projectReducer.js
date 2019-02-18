7aa3fda883d572834ab7b06d5114b7d5c8d54a00
adagar <adagar121987@gmail.com>
2019-02-18 12:15:55 -0500

    12 Adding dummy data
    Added some dummy data to the project reducer. Imported { conect } from react-redux to the project dashboard, used that to map that project data to the props. Passed that down to the project list, which in turned passed individual projects down to the proejct summary. Remember, you send props to class based components and then desctructre them into items like const ProjectSummary = ({ project }) => {}

--- react-redux-app/react-planner/src/store/reducers/projectReducer.js
+++ react-redux-app/react-planner/src/store/reducers/projectReducer.js
@@ -1,4 +1,14 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "find the heart key", content: "blah blah blah" },
+    {
+      id: "2",
+      title: "get the second electric component",
+      content: "blah blah blah"
+    },
+    { id: "3", title: "install the large gear", content: "blah blah blah" }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
