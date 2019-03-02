63c2dff1938b54efda23c24d77972afa5760dda8
adagar <adagar121987@gmail.com>
2019-03-02 14:57:59 -0500

    20 Project Details Page
    Did similar thing that was done to Dashboard to Project Details. Imported compose, connect, and fireStoreConnect. Connected the firestore to the local store, and then pulled that info down into our details page

--- react-redux-app/react-planner/src/components/projects/ProjectDetails.js
+++ react-redux-app/react-planner/src/components/projects/ProjectDetails.js
@@ -1,26 +1,49 @@
 import React from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 const ProjectDetails = (props) => {
-  const id = props.match.params.id;
-  return (
-    <div className="container section project-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Project Title - {id}</span>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
-            veniam dolorum maiores molestias cupiditate sed deserunt? Debitis
-            facere quasi corrupti sapiente in, amet ex asperiores quia sint
-            possimus. Voluptates, qui!
-          </p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by Nick</div>
-          <div>8th of February, 10am</div>
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              Posted by {project.authorFirstName} {project.authorLastName}
+            </div>
+            <div>8th of February, 10am</div>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    );
+  }
 };
 
-export default ProjectDetails;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  const projects = state.firestore.data.projects;
+  const project = projects ? projects[id] : null;
+  console.log(state);
+  return { project: project };
+};
+
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([
+    {
+      collection: "projects"
+    }
+  ])
+)(ProjectDetails);
