cb4081edb1fe0fd26e72f83182884fee5b6d809f
adagar <adagar121987@gmail.com>
2019-03-03 18:23:10 -0500

    23 Connected to Firebase auth and logging in
    Added Firebase Auth to the root reducer, and expanded the auth reducer to actually handle dispatches. In SignIn we brought in connect to connect a mapStateToProps and mapDispatch to props. These dispatch a message to signIn (a new action) that sends the information to firebase auth.

--- react-redux-app/react-planner/src/components/auth/SignIn.js
+++ react-redux-app/react-planner/src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn } from "../../store/actions/authActions";
 
 class SignIn extends Component {
   state = {
@@ -7,7 +9,6 @@ class SignIn extends Component {
   };
 
   handleChange = (e) => {
-    console.log(e);
     this.setState({
       [e.target.id]: e.target.value
     });
@@ -15,11 +16,11 @@ class SignIn extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
 
   render() {
+    const { authError } = this.props;
     return (
       <div>
         <div className="container">
@@ -40,6 +41,10 @@ class SignIn extends Component {
             <div className="input-field">
               <button className="btn pink lighten-1 z-depth-0">Login</button>
             </div>
+
+            <div className="red-text center">
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </form>
         </div>
       </div>
@@ -47,4 +52,19 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  };
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authError: state.auth.authError
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignIn);
