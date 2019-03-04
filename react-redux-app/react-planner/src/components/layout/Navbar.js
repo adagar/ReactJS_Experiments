f4c845166b28e3c3c209cb6d72bd533273d2a6d5
adagar <adagar121987@gmail.com>
2019-03-04 18:18:48 -0500

    25 Tracking Auth Status
    Using the auth status (got from mapStateToProps in Navbar, pulled out of state.firebase.auth) we show the signed in links or the signed out links depending on the user status. It works

--- react-redux-app/react-planner/src/components/layout/Navbar.js
+++ react-redux-app/react-planner/src/components/layout/Navbar.js
@@ -4,22 +4,25 @@ import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
-const NavBar = () => {
+const NavBar = (props) => {
+  const { auth } = props;
+  console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           Mario Plan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth
+  };
 };
 export default connect(mapStateToProps)(NavBar);
--- react-redux-app/react-planner/src/components/layout/SignedInLinks.js
+++ react-redux-app/react-planner/src/components/layout/SignedInLinks.js
@@ -10,7 +10,7 @@ const SignedInLinks = (props) => {
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <a onClick={props.signOut}>Log Out</a>
+        <button onClick={props.signOut}>Log Out</button>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
