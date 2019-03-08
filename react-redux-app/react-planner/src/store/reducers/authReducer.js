72afa1e88895e5e26bdae0a2bbda77e7fbc102e0
adagar <adagar121987@gmail.com>
2019-03-07 19:45:23 -0500

    28 Signing up from website
    Using similar logic as our signin page, we can now sign up from the signup page. Added a signUp action that adds a new user to firebase and uses that UID (from the response) to create a new item in the user collection (where we'll store other info later). Also added an error item similar to the signin

--- react-redux-app/react-planner/src/store/reducers/authReducer.js
+++ react-redux-app/react-planner/src/store/reducers/authReducer.js
@@ -28,6 +28,18 @@ const authReducer = (state = initState, action) => {
         ...state,
         authError: "Sign out failure"
       };
+    case "SIGNUP_SUCCESS":
+      console.log("Sign up success");
+      return {
+        ...state,
+        authError: null
+      };
+    case "SIGNUP_ERROR":
+      console.log("Sign up error");
+      return {
+        ...state,
+        authError: action.err.message
+      };
     default:
       return state;
   }
