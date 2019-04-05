9e4ebc639e2abd61946d8e2b6ab5a83de44bc410
adagar <adagar121987@gmail.com>
2019-04-05 18:31:28 -0400

    Some small tweaks

---
+++ react-redux-app/react-planner/dist/precache-manifest.a41a3975889ad5256b3524517c889f49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4881abaa89e149c1fac4",
+    "url": "/static/js/main.045a03e7.chunk.js"
+  },
+  {
+    "revision": "7350c28a2bd8d6854837",
+    "url": "/static/js/2.64a36c11.chunk.js"
+  },
+  {
+    "revision": "4881abaa89e149c1fac4",
+    "url": "/static/css/main.832fe558.chunk.css"
+  },
+  {
+    "revision": "b8d7c03e8fd668f18bbccbf2c1459d6e",
+    "url": "/index.html"
+  }
+];
